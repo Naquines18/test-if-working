6ef59d3336a5667a27940f59fe74d05d5c2d6df6
Naquines18 <88887528+Naquines18@users.noreply.github.com>
2021-11-15 17:57:41 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 // kani kay ang database credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "barangaydb";
+$servername = "sql6.freemysqlhosting.net";
+$username = "sql6451248";
+$password = "BMKHYVx4l4";
+$dbname = "sql6451248";
 $port = 3306;
 
 // Create connection
