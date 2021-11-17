a5e634b583287b3dc91846bb195daf8377eae3cd
Naquines18 <88887528+Naquines18@users.noreply.github.com>
2021-11-17 11:43:49 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,10 +1,10 @@
 <?php
 // kani kay ang database credentials
-$servername = "sql6.freemysqlhosting.net";
-$username = "sql6451248";
-$password = "BMKHYVx4l4";
-$dbname = "sql6451248";
-$port = 3306;
+$servername = "mysql-58215-0.cloudclusters.net";
+$username = "admin";
+$password = "v4MMEcpC";
+$dbname = "barangaydb";
+$port = 17012;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname,$port);
